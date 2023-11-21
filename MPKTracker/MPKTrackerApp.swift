e225ccde0061436810f061f66503327805f73fe5
Adam Tokarski <a.tokarski126@gmail.com>
2023-11-21 16:53:11 +0100

    Initial Commit

---
+++ MPKTracker/MPKTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPKTrackerApp.swift
+//  MPKTracker
+//
+//  Created by Adam Tokarski on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MPKTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
